21e64c18f881285b79faebff476af572f840ba26
Dirk Ruschhaupt <dirkruschhaupt@Dirks-MacBook-Pro.local>
2018-07-30 13:40:31 -0500

    7302018140p

--- parseHeaders.js
+++ parseHeaders.js
@@ -3,8 +3,27 @@
 /* eslint-env node */
 
 function parseHeaders(text) {
-    // your code here
-    return text;
+    let obj = {};
+    obj.headers = {};
+    obj.code = {};
+    let parsedLines = text.split('\n');
+
+    for (let i = 0; i < parsedLines.length; i++) {
+      if (i === 0) {
+          let versionStatusData = parsedLines[i].split(' ');
+          obj.version = versionStatusData[0];
+          obj.code = versionStatusData[1];
+      }
+      else {
+        let line = parsedLines[i].split(': ');
+        if (line.length === 2) {
+          obj.headers[line[0]] = line[1];
+        }
+      }
+      obj.body = parsedLines.join();
+    }
+
+    return obj;
 }
 
 module.exports = parseHeaders;
